bb1b741585b0415ded24136d0f654db3709334d4
mahi <mahi@mahi-Latitude-7480>
2024-07-30 21:41:22 +0530

    single

--- firstpro/src/inheritance/Single.java
+++ firstpro/src/inheritance/Single.java
@@ -1,5 +1,18 @@
 package inheritance;
 
 public class Single {
+        void eat()
+		{System.out.println("eating...");}  
+		}  
+		class Dog extends Single{  
+		void bark()
+		{System.out.println("barking...");}  
+		}  
+		class TestInheritance{  
+		public static void main(String args[]){  
+		Dog d=new Dog();  
+		d.bark();  
+		d.eat();  
+		}}  
 
-}
+
