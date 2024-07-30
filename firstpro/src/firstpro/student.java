870df663767f8fd0363d2ffdd19cd8b9ec7c509a
mahi <mahi@mahi-Latitude-7480>
2024-07-30 19:46:09 +0530

    encasulation

---
+++ firstpro/src/firstpro/student.java
@@ -0,0 +1,35 @@
+package firstpro;
+public class student {
+	int id;
+	String name;
+	String Dept;
+	int per;
+	public String getname() {
+		return name;
+		}
+	public void setname(String name) {
+		this.name=name;
+	}
+	public int getid() {
+		return id;
+		}
+	public void setid(int id) {
+		this.id=id;
+	}
+	public String getDept() {
+		return Dept;
+		}
+	public void setDept(String Dept) {
+		this.Dept=Dept;
+	}
+	public String getper() {
+		return name;
+		}
+	public void setper(int per) {
+		this.per=per;
+	}
+
+}
+
+
+	
---
+++ firstpro/src/firstpro/student2.java
@@ -0,0 +1,20 @@
+package firstpro;
+
+public class student2 {
+
+	public static void main(String[] args) {
+		student student=new student();
+        student.name="john";
+        student.id=401;
+        student.per= 80;
+        student.Dept="cse";
+        System.out.println(student.name);
+        System.out.println(student.id);
+        System.out.println(student.per);
+        System.out.println(student.Dept);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
