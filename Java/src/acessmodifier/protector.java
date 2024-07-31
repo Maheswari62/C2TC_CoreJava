2f90a2f6ba6b3956c235530fe055472ec68b4cda
mahi <mahi@mahi-Latitude-7480>
2024-07-31 20:52:43 +0530

    protector

---
+++ Java/src/acessmodifier/protector.java
@@ -0,0 +1,14 @@
+package acessmodifier;
+public class protector {
+	public class A{  
+	protected void msg(){System.out.println("Hello");}  
+	}  
+  
+class B extends A{  
+	  public static void main(String args[]){  
+	   B obj = new B();  
+	   obj.msg();  
+	  }  
+	}  
+
+}
