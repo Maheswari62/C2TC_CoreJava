a757cee915429e74b806d39f51edb5ea46f4cad5
mahi <mahi@mahi-Latitude-7480>
2024-07-31 20:36:22 +0530

    overloading

---
+++ firstpro/src/polymorphism/methodoverloading.java
@@ -0,0 +1,13 @@
+package polymorphism;
+public class methodoverloading{ 
+	static int add(int a,int b){return a+b;}  
+	static int add(int a,int b,int c){return a+b+c;} 
+	} 
+class TestOverloading1{  
+public static void main(String[] args){  
+System.out.println(methodoverloading.add(11,11));  
+System.out.println(methodoverloading.add(11,11,11));  
+}}   
+	
+
+	
