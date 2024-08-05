0b40b609bf4f6146e56319592ac59d1c80d0ebc3
Maheswari62 <maheswarikumar58@gmail.com>
2024-08-05 21:14:11 +0530

    new modifie

--- Java/src/acessmodifier/protacted.java
+++ Java/src/acessmodifier/protacted.java
@@ -2,9 +2,7 @@ package acessmodifier;
 public class protacted {
 protected void msg(){System.out.println("Hello");}  
 	}  
-	package acessmodifier;  
-	  
-	class B extends protacted{  
+class B extends protacted{  
 	  public static void main(String args[]){  
 	   B obj = new B();  
 	   obj.msg();  
